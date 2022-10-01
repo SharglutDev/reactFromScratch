eff84ee8c5be54d5789b83a9e4ca2e10a4416de3
sharglut <p.filimon75@gmail.com>
2022-10-01 20:53:34 +0200

    dependancies installed + navigation and logo components

---
+++ src/pages/About.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A PROPOS</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quasi
+        officia quas, amet molestiae repellendus voluptate corrupti ea magnam
+        ratione! Dolorem, sit minima ab ullam omnis quam ipsum ad aperiam
+        quaerat? Deleniti expedita rerum repudiandae ea asperiores atque dolore
+        corrupti rem necessitatibus a pariatur magnam dolor dolores quos ratione
+        dolorem fuga officia laudantium odio voluptates, officiis sed nemo sint.
+        Saepe suscipit natus, adipisci quia tenetur sint dolorum eveniet aperiam
+        neque id nisi magnam est dolor odit facere voluptatem veniam quisquam
+        similique ullam in quaerat eum fugit perferendis. Omnis ducimus ipsa ut
+        inventore nemo, quo rem similique aliquam placeat, fugiat sapiente.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur velit
+        laborum expedita, eaque praesentium quo saepe hic. Aliquam autem
+        veritatis, dolor dignissimos tempora nam possimus delectus, architecto
+        minus atque doloremque excepturi dolorem amet! Tenetur rem eum nisi
+        animi exercitationem magnam quis, velit recusandae hic temporibus
+        excepturi architecto repellendus quisquam dolore esse, amet molestias
+        cupiditate ducimus optio ex cum. Dignissimos exercitationem amet maiores
+        eligendi asperiores accusamus ut mollitia saepe laudantium quos ad ipsa
+        vero dolores pariatur officiis, quaerat veritatis, aperiam ratione nisi
+        culpa aut maxime sapiente. Ipsam dolorum eaque expedita temporibus.
+        Perspiciatis, nulla reiciendis tenetur illum aliquam dolorum, laboriosam
+        repellat magnam eos quam, debitis officia quibusdam ipsam cum eligendi
+        ducimus. Fuga debitis velit architecto veritatis id quibusdam dicta
+        praesentium error tempore culpa assumenda laborum consequatur quidem
+        nobis ipsa aliquid, enim optio iste. Ullam porro aliquam dolore nam
+        commodi aut ipsa, eveniet unde quibusdam repellendus corporis dicta
+        obcaecati a rerum tenetur omnis provident distinctio consequuntur illo.
+        Beatae nobis, quo modi magni ipsum distinctio ea velit. Sint harum
+        repellat deserunt, et cupiditate eum libero fugit totam, veritatis
+        tempora quos, deleniti aliquam commodi dolore enim ut. Inventore animi
+        tempore officiis expedita fugiat maxime facere sunt laudantium
+        molestias, quis, repellat nam in iure harum! Voluptas.
+      </p>
+    </div>
+  );
+};
+
+export default About;

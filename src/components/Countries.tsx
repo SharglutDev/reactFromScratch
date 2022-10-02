8412ececba250b683587da867a35035ad592798a
sharglut <p.filimon75@gmail.com>
2022-10-02 20:52:11 +0200

    add filter & sort logic

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -4,6 +4,16 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState<any>([]);
+  const [rangeValue, setRangeValue] = useState<any>(36);
+  const [selectedRadio, setSelectedRadio] = useState<string>("");
+  const radios: string[] = [
+    "Africa",
+    "North America",
+    "South America",
+    "Asia",
+    "Europe",
+    "Oceania",
+  ];
 
   // Le useEffect se joue lorsque le composant est monté
   useEffect(() => {
@@ -20,12 +30,46 @@ const Countries = () => {
   return (
     <div className="countries">
       <ul className="radio-container">
-        <input type="range" min="1" max="250" />
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent, index) => (
+          <li key={index}>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
       </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
       <ul>
-        {data.map((country: any, index: any) => {
-          return <Card key={index} country={country} />;
-        })}
+        {data
+          .filter((filteredCountry: any) =>
+            selectedRadio
+              ? filteredCountry.continents[0] === selectedRadio
+              : filteredCountry
+          )
+          .sort(
+            (country1: any, country2: any) =>
+              country2.population - country1.population
+          )
+          .slice(0, rangeValue)
+          .map((country: any, index: any) => {
+            return <Card key={index} country={country} />;
+          })}
       </ul>
     </div>
   );
